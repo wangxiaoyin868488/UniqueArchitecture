04721111fc4dc19e5b9e4ca593c23e18dfe641aa
guo <389878212@qq.com>
2015-05-08 13:49:29 +0800

    Initial Commit

---
+++ UniqueArchitecture/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/8.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
