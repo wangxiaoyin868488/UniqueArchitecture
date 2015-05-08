0841fea0e3d056c38bb434f06d0185c118e89bab
guo <389878212@qq.com>
2015-05-08 18:40:06 +0800

    des
    des

---
+++ UniqueArchitecture/UABase/UATableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UATableViewController.h
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/8.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UATableViewController : UAViewController
+
+@end
---
+++ UniqueArchitecture/UABase/UAViewController.m
@@ -0,0 +1,13 @@
+//
+//  UAViewController.m
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/8.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import "UAViewController.h"
+
+@implementation UAViewController
+
+@end
