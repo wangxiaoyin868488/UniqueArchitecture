036c8fdbd6ffe397cef7b132c64a0bb10b289765
guo <389878212@qq.com>
2015-05-10 16:44:46 +0800

    des
    des

---
+++ UniqueArchitecture/UABase/UATabBarController.h
@@ -0,0 +1,13 @@
+//
+//  UATabBarController.h
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/10.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UATabBarController : UITabBarController
+
+@end
---
+++ UniqueArchitecture/UABase/UATabBarController.m
@@ -0,0 +1,13 @@
+//
+//  UATabBarController.m
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/10.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import "UATabBarController.h"
+
+@implementation UATabBarController
+
+@end
