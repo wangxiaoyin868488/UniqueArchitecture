ef1a08f4efeab67c06b49097fd8254483a03fff8
guo <389878212@qq.com>
2015-05-11 19:19:32 +0800

    odbc
    odbc

---
+++ UniqueArchitecture/UABaseDao.h
@@ -0,0 +1,19 @@
+//
+//  UABaseDao.h
+//  UniqueArchitecture
+//
+//  Created by ccguo on 15/5/9.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UABaseDao : NSObject
+
+- (instancetype)shareInstance;
+
+//init db
+//create table
+//insert delete update select
+
+@end
