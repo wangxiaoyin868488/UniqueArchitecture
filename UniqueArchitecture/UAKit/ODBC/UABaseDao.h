8f1d2678093f2739fb10d7f48d0f7de84342123c
guo <389878212@qq.com>
2015-05-12 19:34:00 +0800

    des
    des

---
+++ UniqueArchitecture/UAKit/ODBC/UABaseDao.h
@@ -0,0 +1,19 @@
+//
+//  UABaseDao.h
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/12.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UABaseDao : NSObject
+
+- (instancetype)shareInstance;
+
+//init db
+//create table
+//insert delete update select
+
+@end
---
+++ UniqueArchitecture/UAKit/ODBC/UABaseDao.m
@@ -0,0 +1,44 @@
+//
+//  UABaseDao.m
+//  UniqueArchitecture
+//
+//  Created by summer on 15/5/12.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import "UABaseDao.h"
+#import <FMDB/FMDB.h>
+
+@interface UABaseDao(){
+    FMDatabase *_db;
+}
+
+@end
+
+static UABaseDao *shareInatance = nil;
+
+@implementation UABaseDao
+
+- (instancetype)shareInstance{
+    @synchronized(self){
+        if (shareInatance == nil) {
+            shareInatance = [[UABaseDao alloc] init];
+        }
+    }
+    return shareInatance;
+}
+
++ (instancetype)allocWithZone:(struct _NSZone *)zone{
+    @synchronized(self){
+        if (shareInatance == nil) {
+            shareInatance = [super allocWithZone:zone];
+        }
+    }
+    return nil;
+}
+
+- (instancetype)copy{
+    return shareInatance;
+}
+
+@end
