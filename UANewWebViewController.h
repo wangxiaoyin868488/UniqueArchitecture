269b98aa06b690a26ce2e44969b2b4555d65aee6
guo <389878212@qq.com>
2015-05-14 23:08:03 +0800

    des
     des

---
+++ UANewWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  UANewWebViewController.h
+//  UniqueArchitecture
+//
+//  Created by ccguo on 15/5/14.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import "UAViewController.h"
+
+@interface UANewWebViewController : UAViewController
+
+@end
---
+++ UANewWebViewController.m
@@ -0,0 +1,21 @@
+//
+//  UANewWebViewController.m
+//  UniqueArchitecture
+//
+//  Created by ccguo on 15/5/14.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import "UANewWebViewController.h"
+
+@implementation UANewWebViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+
+@end
